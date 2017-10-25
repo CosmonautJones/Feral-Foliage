dc72f608346746821960590914fb862347bde421
Jesse Reichel <31119775+KryoKorpz@users.noreply.github.com>
2017-10-24 18:01:47 -0700

    end of day

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Js Components/App';
-import registerServiceWorker from './Js Components/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
